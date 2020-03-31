dfaa2f52fe742096456ceb0ba8b4e0c668d7007f
Jian Tao <jtao@tlogin-0502.cluster>
2020-03-31 18:38:58 -0500

    updated the example. Jian

--- pi.py
+++ pi.py
@@ -1,40 +1,16 @@
-from __future__ import print_function
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import pyspark as spark
+import random
 
-import sys
-from random import uniform
+def inside(p):     
+  x, y = random.random(), random.random()
+  return x*x + y*y < 1
 
-from pyspark import SparkContext
+num_samples = 1000000000
 
-def in_circle(_):
-    x = uniform(-1.0, 1.0)
-    y = uniform(-1.0, 1.0)
-    return x ** 2 + y ** 2 <= 1
+sc = spark.SparkContext(appName="Pi")
 
-if __name__ == "__main__":
-    """
-        Usage: pi
-    """
+count = sc.parallelize(range(0, num_samples)).filter(inside).count()
 
-    sc = SparkContext(appName="PythonPi")
-    n = 1000000000
-    count = sc.parallelize(xrange(1, n + 1)).filter(in_circle).count()
-    print("PI=%.12f" % (4.0 * count / n))
-    print("N=%i" % count)
+pi = 4 * count / num_samples
+print(pi)
 
-    sc.stop()
